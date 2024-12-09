0a3b6e401c339a5b0d8807142337a7d2d72ee67d
Stefan Luchs <stefan.luchs@if.de>
2024-12-09 11:11:00 +0100

    Day 8 - 1

--- src/main/kotlin/me/luchs/aoc2024/DayEight.kt
+++ src/main/kotlin/me/luchs/aoc2024/DayEight.kt
@@ -4,36 +4,34 @@ data class DayEight(val input: String) {
 
     private val matrix = input.toCharMatrix()
 
-    fun partOne(): Int {
-        return matrix.distinctValues()
+    fun partOne(): Int =
+        matrix.distinctValues()
             .asSequence()
             .filterNot { '.' == it }
-            .map { it to matrix.entries(it).toList() }
-            .flatMap { antinodesForPosition(it) }
+            .map { matrix.entries(it).toList() }
+            .flatMap { antinodesForChar(it) }
             .distinct().count()
-    }
 
-    private fun antinodesForPosition(position: Pair<Char, List<Coordinate2D>>): List<Coordinate2D> {
-        val antinodes = position.second.combinations().flatMap { antinodesForPair(it) }.distinct()
-        return antinodes
-    }
+    private fun antinodesForChar(coordinates: List<Coordinate2D>): List<Coordinate2D> =
+        coordinates.combinations()
+            .flatMap { antinodesForPair(it) }
+            .distinct()
 
 
     private fun antinodesForPair(pair: Pair<Coordinate2D, Coordinate2D>): List<Coordinate2D> {
-
         val (rows, columns) = matrix.dimensions()
 
+        // construct vector from pair of coordinates
         val vector = pair.second.first - pair.first.first to pair.second.second - pair.first.second
 
-        val options: MutableList<Pair<Int, Int>> = mutableListOf()
-
-        val next = listOf(
+        // antinodes are always on locations which are described as the vector sum of the pair +- the vector
+        val antinodes = listOf(
             pair.second.first + vector.first to pair.second.second + vector.second,
             pair.first.first - vector.first to pair.first.second - vector.second
-        ).filter { it.first in rows && it.second in columns }
-        options.addAll(next)
-
-        val antinodes = options.filterNot { it == pair.first }.filterNot { it == pair.second }
+        )
+            .filter { it.first in rows && it.second in columns } // must be within bounds
+            .filterNot { it == pair.first }
+            .filterNot { it == pair.second }
 
         return antinodes
     }
